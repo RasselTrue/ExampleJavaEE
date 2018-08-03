915d62255625e16321b2b0bab9de74d6fc63a3f0
RasselTrue <danss@bk.ru>
2018-08-03 19:35:51 +0800

    Домашняя работа №2. Сервлеты, фильтры

--- src/main/java/ru/dansaranov/enterprise/servlets/MainServlet.java
+++ src/main/java/ru/dansaranov/enterprise/servlets/MainServlet.java
@@ -1,34 +1,28 @@
 package ru.dansaranov.enterprise.servlets;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = MainServlet.NAME, urlPatterns = {"/main"})
 public class MainServlet extends HttpServlet {
+
+    public static final String NAME = "MainServlet";
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/main.jsp");
+        dispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        doGet(req, resp);
     }
 
-    @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
-    }
 
-    @Override
-    public void destroy() {
-        super.destroy();
-    }
-
-    @Override
-    public void init() throws ServletException {
-        super.init();
-    }
 }
